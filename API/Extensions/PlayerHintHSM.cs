4ddb4568bcfa6664b8dee053dd5e403a42faf25a
FentanylReactorGER <fentanylreactor@gmail.com>
2024-12-25 14:02:18 +0100

    Update PlayerHintHSM.cs

--- API/Extensions/PlayerHintHSM.cs
+++ API/Extensions/PlayerHintHSM.cs
@@ -5,7 +5,7 @@ namespace Fentanyl_ReactorUpdate.API.Extensions
 {
     public static class PlayerExtensions
     {
-        public static void ShowMeowHint(this Player player, string text)
+        public static void ShowMeowHint(this Player player, string text) // Dear EXILED People, this is in work
         {
             player.ShowHint(text, Plugin.Singleton.Config.GlobalHintDuration);
         }
